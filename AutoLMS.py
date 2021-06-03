881b2868380336f19e048c964b4c28a86502d5bc
hucce <huccce@gmail.com>
2021-06-03 09:15:10 +0900

    선택창 추가, 팝업창 다른 것도 적용되도록 수정함 수정완료

--- AutoLMS.py
+++ AutoLMS.py
@@ -1,9 +1,5 @@
-import pandas as pd
-import numpy as np
 import re
-import csv
 import time
-import os
 import requests
 import datetime
 
@@ -13,6 +9,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
 
 def AutoLMS(iD, passWord, classNames):
     driver = webdriver.Chrome('./chromedriver')
@@ -52,9 +49,14 @@ def AutoLMS(iD, passWord, classNames):
             link = linkList[0]
             classTxt = link.text
             # 팝업을 끈다
-            btn = driver.find_element_by_xpath('/html/body/div[3]/div[4]/div[1]/button')
-            btn.click()
-            time.sleep(1)
+            popupList = ['modal.notice_popup.ui-draggable', 'modal.popup-pnotice.ui-draggable']
+            for popupClass in popupList:
+                popups = driver.find_elements_by_class_name(popupClass)
+                for popup in popups:
+                    btn = popup.find_element(By.TAG_NAME, 'button')
+                    btn.click()
+                    time.sleep(1)
+
             link.click()
             time.sleep(2)
             # Vod 리스트 확인
@@ -110,4 +112,27 @@ def AutoLMS(iD, passWord, classNames):
                                 linkList.append(data)
 
 # ['폭력예방', '안전교육']
-AutoLMS('20215349', 'gjals9wkd!3', ['안전교육'])
+
+id = input("아이디를 입력하세요: ")
+password = input("비밀번호를 입력하세요: ")
+print("받으실 교육을 선택해주세요")
+print("1. 폭력예방, 2. 안전교육, 3. 둘다")
+
+for i in range(0,10):
+    sel = input("교육 선택: ")
+
+    leanList = []
+    if sel == '1':
+        leanList.append("폭력예방")
+        break
+    elif sel == '2':
+        leanList.append("안전교육")
+        break
+    elif sel == '3':
+        leanList.append("안전교육")
+        leanList.append("폭력예방")
+        break
+    else:
+        print('입력에 문제가 있습니다. 다시 입력해주세요.')
+
+AutoLMS(id, password, leanList)
